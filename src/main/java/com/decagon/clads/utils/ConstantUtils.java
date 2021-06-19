bf13544a07d258bfae2d65176786d4bcbebc37a9
darot <darotudeen@gmail.com>
2021-06-19 17:16:01 +0100

    update client id

--- src/main/java/com/decagon/clads/utils/ConstantUtils.java
+++ src/main/java/com/decagon/clads/utils/ConstantUtils.java
@@ -13,7 +13,7 @@ public class ConstantUtils {
     public static final String PHONE_NUMBER_PATTERN = "^0(80|70|90|81)([12356709])\\d{7}$";
     public static final String PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
     public static final String BASE_URL = "/api/v1";
-    public static final String CLIENT_ID ="525232674561-10iuvisduhg0ebrjgksg0cpd7mdlr0nj.apps.googleusercontent.com";
+    public static final String CLIENT_ID ="525232674561-jqs92483fpb2j2dv6qustgik08rkkoiu.apps.googleusercontent.com";
     @Value("${host.base}")
     public String host;
 }
